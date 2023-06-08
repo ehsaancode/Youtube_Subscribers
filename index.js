662d3fbffebc5626725045107bf12973e665ec6b
Ehsaan Mondal <mail.ehsaan@gmail.com>
2023-06-09 00:24:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const app = require('./app.js')
 const mongoose = require('mongoose')
 const dotenv = require("dotenv")
@@ -7,19 +6,17 @@ const dotenv = require("dotenv")
 //configuring env
 dotenv.config();
 
-
-
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
-// Connect to DATABASE
+// Connecting to DATABASE using ENV
 const DATABASE_URL = process.env.MONGOURI;
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
 db.once('open', () => console.log('connected to database'))
 
-// Start Server
+// To start the Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
